d48eac4c541c8d5666f852913fd1590b48c73793
Kacper <kacpercybulski@icloud.com>
2024-01-20 18:12:25 +0100

    Challenge is finished

--- rpg-character-creator/src/App.jsx
+++ rpg-character-creator/src/App.jsx
@@ -7,53 +7,27 @@ import Button from './components/Button'
 import attackOptionsList from '../data/attackOptionsList'
 import nameList from '../data/namesList'
 
-function App() {
-	/* Challenge
+import { useState } from 'react'
 
-    This video game character creator needs a starting character! Your task is to build one by doing the following: 
-    
-        1. Declare a state called characterData and a state setting function called 
-           setCharacterData. 
-           
-        2. Make the value of the initial characterData state be an object with the following 
-           properties and values.
-           
-        		Property		 	       Values  				  
-			╷---------------╷------------------------------------╷					
-		  	| hat           |	true or false                    |
-			|---------------|------------------------------------|
-			| shield        |	true or false                    |
-			|---------------|------------------------------------|
-            | weapon        |	The string "sword" or "staff" 	 |
-			|---------------|------------------------------------|
-            | name          |	A string* that is the name       | 
-            |               |    of the character.               |
-			|---------------|------------------------------------|
-			| attackOptions |	An array of six strings*, each   |
-            |               |   of which is a name of an attack. |
-            |---------------|------------------------------------|
-			| stats         |	An object with three properties: |
-            |	            |      - hp: x**                     |
-            |               |      - mp: y                       |
-            |               |      - strength: z                 |
-			¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ 	
-            
-                *  You can write your own character name and attack options, or you can use ones 
-                   from the namesList and attackOptionsList arrays, which have already been imported into this file.
-                  
-                ** For the stats, x, y and z should be any numbers between 0 and 100 (inclusive).
-                
-        3. You should see your character with the property values you specified appear on the 
+function App() {
+	const [characterData, setCharacterData] = useState({
+		hat: true,
+		shield: true,
+		weapon: 'sword',
+		name: 'Kylie',
+		attackOptions: ['Fireball', 'Thunder Strike', 'Ice Shard', 'Earthquake', 'Wind Slash', 'Lightning Bolt'],
+		stats: {
+			hp: 100,
+			mp: 50,
+			strength: 50,
+		},
+	})
+
+	/* Your task: 
+
+           You should see your character with the property values you specified appear on the 
            screen when you run your code. You should test your code by manually changing some of the characterData property values and seeing if you get the correct results. You should also click the "Toggle" button, which will randomly change the values by using the state setting function!  
     */
-
-	/* ️⬇️️ ------------------ Write your code below! -----------------️️ ⬇️️ */
-
-	/* ------------------------------------------------------------------
-    
-      ⚠️ ️Write your code above! The code below should not be changed! ⚠️
-    
-    --------------------------------------------------------------------*/
 
 	let dataToUse, functionToUse
 
--- rpg-character-creator/src/components/Character.jsx
+++ rpg-character-creator/src/components/Character.jsx
@@ -1,36 +1,34 @@
-import React from "react"
+import React from 'react'
 
 export default function Character({ characterData }) {
 	if (!characterData.noData) {
 		switch (true) {
 			case characterData.hat === undefined:
-				throw Error("No hat value provided.")
+				throw Error('No hat value provided.')
 			case characterData.shield === undefined:
-				throw Error("No shield value provided.")
+				throw Error('No shield value provided.')
 			case characterData.weapon === undefined:
-				throw Error("No weapon value provided.")
+				throw Error('No weapon value provided.')
 			case characterData.name === undefined:
-				throw Error("No name value provided.")
-			case typeof characterData.name !== "string":
-				throw Error("Invalid name value.")
+				throw Error('No name value provided.')
+			case typeof characterData.name !== 'string':
+				throw Error('Invalid name value.')
 			case characterData.name.trim().length < 2:
-				throw Error("Invalid name length.")
-			case typeof characterData.hat !== "boolean":
-				throw Error("Invalid hat value.")
-			case typeof characterData.shield !== "boolean":
-				throw Error("Invalid shield value.")
-			case characterData.weapon !== "sword" && characterData.weapon !== "staff":
-				throw Error("Invalid weapon value.")
+				throw Error('Invalid name length.')
+			case typeof characterData.hat !== 'boolean':
+				throw Error('Invalid hat value.')
+			case typeof characterData.shield !== 'boolean':
+				throw Error('Invalid shield value.')
+			case characterData.weapon !== 'sword' && characterData.weapon !== 'staff':
+				throw Error('Invalid weapon value.')
 		}
 	}
 
-	const hat = characterData.hat ? "hat" : "noHat"
-	const shield = characterData.shield ? "shield" : "noShield"
+	const hat = characterData.hat ? 'hat' : 'noHat'
+	const shield = characterData.shield ? 'shield' : 'noShield'
 	const weapon = characterData.weapon
 
-	const characterImageUrl = characterData.noData
-		? null
-		: "./images/" + hat + "-" + shield + "-" + weapon + ".png"
+	const characterImageUrl = characterData.noData ? null : './src/assets/' + hat + '-' + shield + '-' + weapon + '.png'
 
 	return (
 		<div className="character-container">
