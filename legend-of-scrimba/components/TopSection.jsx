c73cdf20907dae1055aefd5ea0e553ca6f46737c
Kacper <kacpercybulski@icloud.com>
2024-02-08 22:36:52 +0100

    challegne finished

---
+++ legend-of-scrimba/components/TopSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function TopSection({ submitted }) {
+	const classCondition = document.getElementsByClassName('name-input').length === 0
+	return (
+		<div className="top-section">
+			<h1 className={submitted && !classCondition ? 'second-fade-in' : null}>
+				{!submitted || classCondition ? 'The Legend of Scrimba' : "A hero has risen! The hero's name is: "}
+			</h1>
+			<div className="image-container">
+				<img
+					className={!submitted || classCondition ? null : 'fade-out-and-in'}
+					src={!submitted || classCondition ? './src/assets/character1.png' : './src/assets/character2.png'}
+				/>
+			</div>
+		</div>
+	)
+}
+
+	
+
+
--- legend-of-scrimba/src/App.jsx
+++ legend-of-scrimba/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import TopSection from '../components/TopSection'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import BottomSection from '../components/BottomSection'
+
+export default function App() {
+	const [playerName, setPlayerName] = React.useState('')
+	const [submitted, setSubmitted] = React.useState(false)
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		setSubmitted(true)
+		setPlayerName(prevName => prevName.trim() + ' The Magnificent!')
+	}
+
+	function handleInputChange(e) {
+		setPlayerName(e.target.value)
+	}
+
+	return (
+		<form className="form" onSubmit={handleSubmit}>
+			<TopSection submitted={submitted} />
+
+			{/*-----Your input below!----------*/}
+
+			<input
+				className="name-input"
+				type="text"
+				maxLength={16}
+				placeholder="Enter Your Character's Name"
+				required
+				disabled={submitted}
+				value={playerName}
+				onChange={handleInputChange}
+			/>
+
+			{/*-----Your input above!----------*/}
+
+			<BottomSection submitted={submitted} playerName={playerName} />
+		</form>
+	)
 }
-
-export default App
