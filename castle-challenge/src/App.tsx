25199305072e66a485abc6bec352e1d27e5dcf9e
Kacper <kacpercybulski@icloud.com>
2024-03-03 22:15:29 +0100

    .

--- castle-challenge/src/App.tsx
+++ castle-challenge/src/App.tsx
@@ -6,15 +6,11 @@ import runGame from './utilities/runGame'
 import initialEnemyData from './data/initialEnemyData'
 
 
-
-
-
-
 export default function App() {
 	const [gameRunning, setGameRunning] = useState(false)
 	const [enemiesData, setEnemiesData] = useState(initialEnemyData)
 
-	runGame(gameRunning, setEnemiesData)
+	runGame(gameRunning, setEnemiesData)	
 
 	return (
 		<div className="wrapper">
