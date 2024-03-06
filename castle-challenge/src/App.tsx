b85b03deb126b39a6f54e9aaecc2741fb8a0fc06
Kacper <kacpercybulski@icloud.com>
2024-03-06 23:00:35 +0100

    .

--- castle-challenge/src/App.tsx
+++ castle-challenge/src/App.tsx
@@ -4,6 +4,7 @@ import Enemy from './components/Enemy'
 import Button from './components/Button'
 import runGame from './utilities/runGame'
 import initialEnemyData from './data/initialEnemyData'
+import { nanoid } from 'nanoid'
 
 export default function App() {
 	const [gameRunning, setGameRunning] = useState(false)
@@ -11,46 +12,19 @@ export default function App() {
 
 	runGame(gameRunning, setEnemiesData)
 
-	/* Challenge
-		
-	The castle for this action RPG needs enemies! Your task is to create them as follows: 
-	
-		1. The "castle-container" div should contain 3 Enemy elements, the component for which 
-		   has already been imported above. 
-		
-		2. Each Enemy element should receive 3 props, which, in turn, should receive the 
-		   following values from 1 of the 3 objects of enemy data in the enemiesData state array: 
-			
-			   Props		 	Values (corresponding to*) 				  
-			╷---------------╷-----------------------------╷					
-		  	|  currentImage |	enemies[n].currentImage   |
-			|---------------|-----------------------------|
-			|  position     |	enemies[n].position 	  |
-			|---------------|-----------------------------|
-			|  orientation  |	enemies[n].orientation	  |	
-			¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯	
-								        n = 0, 1, or 2
-										
-										*You can obtain the underlying values however you want!
-			
-		3. In the end, there should be 3 Enemy elements corresponding to each of the 3 
-		   enemiesData objects, resulting in 3 different enemies in three different positions in the castle. When you click the "Play" button, they should start walking back and forth!
-		   
-*/
+	const enemies = enemiesData.map(enemy => (
+		<Enemy
+			key={nanoid()}
+			orientation={enemy.orientation}
+			position={enemy.position}
+			currentImage={gameRunning ? enemy.currentImage : enemy.altImage}
+		/>
+	))
 
 	return (
 		<div className="wrapper">
 			<div className="meadow-container">
-				<div className="castle-container">
-					{enemiesData.map(enemy => (
-						<Enemy
-							key={enemy.id}
-							position={enemy.position}
-							orientation={enemy.orientation}
-							currentImage={gameRunning ? enemy.currentImage : enemy.altImage}
-						/>
-					))}
-				</div>
+				<div className="castle-container">{enemies}</div>
 			</div>
 
 			<Button stateProps={{ gameRunning, setGameRunning }} />
