c9c5237374e9dfdaf55ac3e4896a0d26cf08cc47
Kacper <kacpercybulski@icloud.com>
2024-04-03 20:44:38 +0200

    task is finished

---
+++ stock-tracker/src/components/Stock.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Stock(props) {
+	const { stock } = props
+
+	const { stockName, currentPrice, prevClosingPrice, logo } = stock
+
+	const numericalChange = (parseFloat(currentPrice) - parseFloat(prevClosingPrice)).toFixed(2)
+
+	const rateChange = ((numericalChange / parseFloat(prevClosingPrice)) * 100).toFixed(2)
+	const colorClass = numericalChange > 0 ? 'green' : numericalChange < 0 ? 'red ' : undefined
+	const arrow = numericalChange > 0 ? '⬆' : numericalChange < 0 ? '⬇ ' : '▬'
+
+	return (
+		<div className="stock-container">
+			<div className={colorClass}>
+				<p>
+					{arrow}
+					{Math.abs(numericalChange)}
+				</p>
+				<p>{rateChange}%</p>
+			</div>
+			<div>
+				<img className="logo" src={logo} alt="Logo" />
+			</div>
+			<div>
+				<p>{stockName}</p>
+			</div>
+			<div>
+				<p>${currentPrice}</p>
+				<p>Current Price</p>
+			</div>
+			<div>
+				<p>Previous Close: ${prevClosingPrice}</p>
+			</div>
+		</div>
+	)
+}
---
+++ stock-tracker/src/data/stockData.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		stockName: 'AAPL',
+		logo: './src/assets/apple.svg',
+		currentPrice: 153.83,
+		prevClosingPrice: 151.03,
+	},
+	{
+		stockName: 'META',
+		logo: './src/assets/meta.svg',
+		currentPrice: 184.91,
+		prevClosingPrice: 185.25,
+	},
+	{
+		stockName: 'GOOGL',
+		logo: './src/assets/google.svg',
+		currentPrice: 95.13,
+		prevClosingPrice: 92.65,
+	},
+	{
+		stockName: 'AMZN',
+		logo: './src/assets/amazon.svg',
+		currentPrice: 93.75,
+		prevClosingPrice: 94.91,
+	},
+	{
+		stockName: 'MSFT',
+		logo: './src/assets/microsoft.svg',
+		currentPrice: 256.85,
+		prevClosingPrice: 256.85,
+	},
+]
