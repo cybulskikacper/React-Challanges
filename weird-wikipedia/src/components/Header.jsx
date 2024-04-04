2502609c6136cd8aa5402d995545ac4cb7e52a50
Kacper <kacpercybulski@icloud.com>
2024-04-04 18:46:04 +0200

    .

---
+++ weird-wikipedia/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<header>
+			<img src="./src/assets/wikipedia-w.svg" />
+			<h1>Weird Wikipedia Articles</h1>
+		</header>
+	)
+}
