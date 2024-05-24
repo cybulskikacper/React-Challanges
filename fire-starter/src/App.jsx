fda3398a2387bcde31b7a3c9a03991fac23e465d
Kacper <kacpercybulski@icloud.com>
2024-05-24 17:38:15 +0200

    finished task

--- fire-starter/src/App.jsx
+++ fire-starter/src/App.jsx
@@ -6,7 +6,7 @@ import Torch from './components/Torch'
 import runBackgroundEffects from '/utilities/runBackgroundEffects'
 
 export default function App() {
-	const [torchEquipped, setTorchEquipped] = React.useState()
+	const [torchEquipped, setTorchEquipped] = React.useState(false)
 	const [woodKindling, setWoodKindling] = React.useState(false)
 	const [woodOnFire, setWoodOnFire] = React.useState(false)
 
@@ -21,37 +21,37 @@ export default function App() {
 		left: cursorPosition.x - 10,
 		top: cursorPosition.y - 60,
 	}
-	/*----------------------------------------------------------------- */
 
-	/* Challenge:
+	function toggleTorch() {
+		setTorchEquipped(!torchEquipped)
+	}
 
-	The user needs to be able to pick up the torch and use it to light the wood on fire! Your task is to allow them to do as follows:  
+	const handleMouseEnter = () => {
+		if (torchEquipped) {
+			setWoodKindling(true)
+		}
+	}
 
-      2. The torchEquipped state should be set to false whenever the user's mouse button is up and 
-         their cursor is anywhere inside the "wrapper" div (line 54).
-      
-      3. The woodKindling state should be set to true when *all* of the following conditions 
-         are met: 
-          - torchEquipped is true 
-          - the user's cursor has entered the "wood-container" div (line 64)
-        
-      4. The woodOnFire state should be set to true when *all* of the following conditions are met:
-          - torchEquipped is true
-          - woodKindling is true
-          - the user's cursor has left the "wood-container" div (line 64) 
-  */
+	const handleMouseLeave = () => {
+		if (torchEquipped && woodKindling) {
+			setWoodOnFire(true)
+		}
+	}
 
 	return (
-		<div onMouseUp={() => setTorchEquipped(false)} className={`wrapper ${torchEquipped && 'relative no-cursor'}`}>
+		<div onMouseUp={toggleTorch} className={`wrapper ${torchEquipped && 'relative no-cursor'}`}>
 			<div className={`game-area ${!torchEquipped && 'relative'}`}>
 				<div
 					className={`torch-container ${torchEquipped && 'torch-equipped'}`}
 					style={torchEquipped ? torchStyle : null}
-					onMouseDown={() => setTorchEquipped(true)}>
+					onMouseDown={toggleTorch}>
 					<Torch />
 				</div>
 
-				<div className={`wood-container ${kindleClass}`}>
+				<div
+					className={`wood-container ${kindleClass}`}
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}>
 					🪵
 					<Fire woodOnFire={woodOnFire} />
 				</div>
