891cd67fa34522b432fbab7c104a878c1ee16be1
Kacper <kacpercybulski@icloud.com>
2024-05-27 21:17:56 +0200

    working on a task

---
+++ frontend-birthday/src/components/InnerMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function InnerMessage() {
+	return (
+		<div className="inner-container">
+			<img className="inner-img" src="./src/assets/InnerCardContent.png" />
+			<div className="balloon">🎈</div>
+		</div>
+	)
+}
