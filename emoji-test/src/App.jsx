bd84f12cd249bb0a18143f4589d26b9a991e45d7
Kacper <kacpercybulski@icloud.com>
2024-06-29 18:25:47 +0200

    updated handleClick function

--- emoji-test/src/App.jsx
+++ emoji-test/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import ResultsModal from './components/ResultsModal'
+import EmojiLists from './components/EmojiLists'
+import emojis from './data/emojis'
+import { nanoid } from 'nanoid'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const [likedEmojis, setLikedEmojis] = React.useState([])
+	const [passedEmojis, setPassedEmojis] = React.useState([])
+	const [currentEmojis, setCurrentEmojis] = React.useState(() => getRandomEmojis())
+	const [showResults, setShowResults] = React.useState(false)
+	const [resultsReady, setResultsReady] = React.useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function handleClick(emoji) {
+		setLikedEmojis(prevLikedEmojis => [...prevLikedEmojis, emoji])
+
+		const nonClickedEmojis = currentEmojis.filter(e => e !== emoji)
+		setPassedEmojis(prevPassedEmojis => [...prevPassedEmojis, ...nonClickedEmojis])
+
+		setCurrentEmojis(() => getRandomEmojis())
+	}
+
+	function getRandomEmojis() {
+		function chooseRandomEmoji() {
+			return emojis[Math.floor(Math.random() * emojis.length)]
+		}
+		return new Array(3).fill('').map(item => chooseRandomEmoji())
+	}
+
+	function getResults() {
+		setShowResults(true)
+	}
+
+	function reset() {
+		setLikedEmojis([])
+		setPassedEmojis([])
+		setShowResults(false)
+		setResultsReady(false)
+	}
+
+	React.useEffect(() => {
+		showResults &&
+			setTimeout(() => {
+				setResultsReady(true)
+			}, 2000)
+	}, [showResults])
+
+	function generateListItems(element) {
+		return <li key={nanoid()}>{element}</li>
+	}
+
+	return (
+		<div className="wrapper">
+			<div className="results-counter">{likedEmojis.length} / 10</div>
+
+			<ResultsModal
+				showResults={showResults}
+				getResults={getResults}
+				resultsReady={resultsReady}
+				reset={reset}
+				generateListItems={generateListItems}
+				likedEmojis={likedEmojis}
+			/>
+
+			<h1>Emoji Personality Test</h1>
+
+			<div className="overall-emojis-container">
+				<button onClick={() => handleClick(currentEmojis[0])}>{currentEmojis[0]}</button>
+				<button onClick={() => handleClick(currentEmojis[1])}>{currentEmojis[1]}</button>
+				<button onClick={() => handleClick(currentEmojis[2])}>{currentEmojis[2]}</button>
+			</div>
+
+			<EmojiLists likedEmojis={likedEmojis} passedEmojis={passedEmojis} generateListItems={generateListItems} />
+		</div>
+	)
 }
-
-export default App
