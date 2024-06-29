0f90e7d959a541b9e1f7abaf522f14362fc7e3ce
Kacper <kacpercybulski@icloud.com>
2024-06-29 13:42:01 +0200

    sonnets will be hidden by deafult

--- sonnet-central/src/App.jsx
+++ sonnet-central/src/App.jsx
@@ -8,12 +8,13 @@ export default function App() {
 	const [searchInput, setSearchInput] = React.useState('')
 
 	function handleClick() {
-		setSearchInput(inputRef.current.value.trim())
+		const input = inputRef.current.value.trim()
+		setSearchInput(input)
 	}
 
-	const filtredSonnets = sonnetsData.filter(sonnet =>
-		sonnet.lines.some(line => line.toLowerCase().includes(searchInput.toLowerCase()))
-	)
+	const filteredSonnets = searchInput
+		? sonnetsData.filter(sonnet => sonnet.lines.some(line => line.toLowerCase().includes(searchInput.toLowerCase())))
+		: []
 
 	const highlightSearchTerm = line => {
 		if (!searchInput) return line
@@ -27,39 +28,22 @@ export default function App() {
 			<Header searchProps={{ inputRef, handleClick }} />
 
 			<div className="sonnets-container">
-				{filtredSonnets.map(sonnet => (
-					<div key={nanoid()} className="sonnet">
-						<h2>{sonnet.title}</h2>
-						<h3>{sonnet.number}</h3>
-						{sonnet.lines.map((line, index) => (
-							<p key={index}>{highlightSearchTerm(line)}</p>
-						))}
-					</div>
-				))}
+				{searchInput === '' ? (
+					''
+				) : filteredSonnets.length === 0 ? (
+					<p>Alas, thy search hath yielded no results</p>
+				) : (
+					filteredSonnets.map(sonnet => (
+						<div key={nanoid()} className="sonnet">
+							<h2>{sonnet.title}</h2>
+							<h3>{sonnet.number}</h3>
+							{sonnet.lines.map((line, index) => (
+								<p key={index}>{highlightSearchTerm(line)}</p>
+							))}
+						</div>
+					))
+				)}
 			</div>
 		</div>
 	)
 }
-
-/* Challenge
-
-  When the user clicks the "Search" button, the text they wrote in the input field becomes the value of the searchInput state (this code has already been written). Using this value, your task is to complete the users' search as follows: 
-  
-    1. For each sonnet in the sonnetsData array that contains the searchInput value in one of its 
-       lines, create a div with a className of "sonnet" in the "sonnets-container" div (line 34). 
-    
-    2. In the "sonnet" div, insert the sonnet's number property as the text content 
-       of an <h3> element and then insert *each* line of the sonnet from its lines property/array as the text content of a <p> element, resulting in one <p> per line of the sonnet. 
-       
-    3. Test your code by searching for common words like "love", "summer", "winter", and "strange" 
-       as well as words that don't appear in any sonnets, like "hello" and "weird."
-*/
-
-/*Bonus Challenges
-      
-    - If there are no search results, render a <p> element in the "sonnets-container" div saying   
-      "Alas, thy search hath yielded no results". 
-      
-    - If there are results, put a <span> around each word in the sonnet that matches the 
-      searchInput value. This will automatically highlight the word (the CSS is already set up). 
-*/
