495685b52e6af97766617acffd57839f645118e0
Kacper <kacpercybulski@icloud.com>
2024-06-29 22:57:14 +0200

    challenge finished

--- employee-spying/src/App.jsx
+++ employee-spying/src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Header from './components/Header'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const [userInput, setUserInput] = React.useState('')
+	const [trackingReports, setTrackingReports] = React.useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const infractionMessage = '🚨🚨🚨 INFRACTION DETECTED! 🚨🚨🚨'
+
+	if (trackingReports.length > 0) {
+		console.log(trackingReports)
+		if (trackingReports[trackingReports.length - 1].infractionDetected) {
+			console.log(infractionMessage)
+		}
+	}
+
+	function getTimeStamp() {
+		const timeStamp = new Date()
+		return (
+			timeStamp.toLocaleTimeString([], {
+				hour: '2-digit',
+				minute: '2-digit',
+				second: '2-digit',
+				hour12: false,
+			}) +
+			'.' +
+			(timeStamp.getMilliseconds() / 1000).toFixed(3).slice(-3)
+		)
+	}
+
+	function handleInputChange(e) {
+		let inputValue = e.target.value
+
+		if (inputValue.includes('Evil Corp.')) {
+			inputValue = inputValue.replace(/Evil Corp./g, 'Good Corp.')
+		}
+
+		setUserInput(inputValue)
+
+		const newReport = {
+			timeStamp: getTimeStamp(),
+			employeeInput: inputValue,
+			infractionDetected: inputValue.includes('Evil Corp'),
+		}
+
+		setTrackingReports(prevReports => [...prevReports, newReport])
+	}
+	return (
+		<div>
+			<Header />
+			<textarea placeholder="Write your report here..." value={userInput} onChange={handleInputChange} />
+		</div>
+	)
 }
 
-export default App
+/* Challenge 
+
+This company wants to spy on its employees. Your task is to help them do the following:
+
+    1. Every time the user types in the textarea, the userInput and trackingReports states should 
+       both be updated. 
+       
+            a. The value of userInput should be a string equaling whatever the user has typed in 
+               the textarea (with one exception, discussed in task 2 below). 
+            
+            b. For the trackingReports state array, any previously-existing objects in the array 
+               should be preserved, and a new object should be added to the end of the array. This object should contain three properties:
+                    
+             Property		 	          Value 				  
+		    	╷----------------------╷-------------------------------------------╷
+		      |  timeStamp           |  return value of getTimeStamp function    |
+		    	|----------------------|-------------------------------------------|
+		    	|  employeeInput       |  All of the current input in the textarea |
+		    	|----------------------|-------------------------------------------|
+		    	|  infractionDetected  |  If employeeInput contains the string     |
+          |                      |  "Evil Corp.", true — otherwise, false	   |	
+		    	¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+           
+    2. If the user writes the string "Evil Corp." (in title case), the string should automatically 
+       be replaced with "Good Corp." in both the userInput state and the textarea. 
+       
+    3. Test out the app by writing "Evil Corp. test" in the textarea. If you complete these tasks 
+       correctly, you should get a console.log message every time you type a letter, and the messages should be like the ones in the sampleOutput.md file.
+       
+    4. You only need to write code below! None of the code above or elsewhere in the project should 
+       be changed.
+*/
