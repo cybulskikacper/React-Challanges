a0a0576d85bda3b1e943ebfb883de8c62a95fffc
Kacper <kacpercybulski@icloud.com>
2024-06-29 14:26:55 +0200

    challenge completed

--- site-enter-btn/src/App.jsx
+++ site-enter-btn/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import WelcomeMessage from './components/WelcomeMessage'
+import generateMessage from './utilities/generateMessage'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const [userData, setUserData] = React.useState({
+		hasEntered: false,
+		pageLoadTime: new Date(),
+		entranceTime: undefined,
+		clickCoordinates: { offsetX: undefined, offsetY: undefined },
+	})
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	if (userData.hasEntered) {
+		generateMessage(userData)
+	}
+
+	function handleEventClick(e) {
+		setUserData(prevState => ({
+			...prevState,
+			hasEntered: true,
+			entranceTime: new Date(),
+			clickCoordinates: { offsetX: e.nativeEvent.offsetX, offsetY: e.nativeEvent.offsetY },
+		}))
+	}
+
+	return (
+		<div>
+			<button
+				onClick={handleEventClick}
+				disabled={userData.hasEntered}
+				className={userData.hasEntered ? 'activated' : 'unactivated'}>
+				{userData.hasEntered ? 'Connecting' : 'Enter'}
+			</button>
+
+			<WelcomeMessage userData={userData} />
+		</div>
+	)
 }
 
-export default App
+/* Challenge
+  
+     This retro VR app needs an "enter" button! The button should be set up as follows:
+     
+        1. If the user clicks the button, the values of the userData state's properties should 
+           become the following: 
+           
+           	                  Property		 	          Value(s)					  
+			            ╷--------------------╷----------------------------------╷
+		  	            |  hasEntered        |	true                            |
+			            |--------------------|----------------------------------|
+			            |  pageLoadTime      |	perserve previous value         |
+			            |--------------------|----------------------------------|
+			            |  entranceTime      |	new date object                 |	
+                        |--------------------|----------------------------------|
+			            |  clickCoordinates  |	obect containing click event's  |
+                        |                    |  offsetX and offsetY values      |	
+			            ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+                        		         
+        2. If you complete these tasks correctly, you should get a button with some retro, glitchy 
+           visual effects, and you should get a correctly rendered message in the console!
+ */
