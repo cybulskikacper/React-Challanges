6c54352f590f383502fb8cb0e2adb5244b2a186d
Kacper <kacpercybulski@icloud.com>
2024-07-04 20:27:35 +0200

    .

--- Debatable/src/components/DebatePost.jsx
+++ Debatable/src/components/DebatePost.jsx
@@ -62,23 +62,3 @@ export default function DebatePost() {
 		</div>
 	)
 }
-
-/* Challenge 
-
-The form doesn't work. Your task is to make it a controlled form that adds a comment to the post when the user clicks "Send."
-
-    1. The comment should appear on the bottom of the comment thread, with the inputted 
-       username and comment text displayed just like the previous comments. 
-       
-    2. The comment should be added to the array that contains the data for the previous 
-       comments. 
-    
-    3. The inputted username should be recorded, but it should show up as "AnonymousUser" if 
-       the user checks the checkbox.
-    
-    4. The user should have to input text into the text input element and comment box element to 
-       submit the form, and the elements and the checkbox should clear out after the user submits a comment. They should be empty on page load as well.   
-        
-    5. Your code can live entirely inside this file, although you are welcome to move things around 
-       if you'd like. 
-*/
