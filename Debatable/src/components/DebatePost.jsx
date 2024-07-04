2f1f4cd6146436ad81712b971bfa3cadedb3fe59
Kacper <kacpercybulski@icloud.com>
2024-07-04 14:11:04 +0200

    working on a new challenge

--- Debatable/src/components/DebatePost.jsx
+++ Debatable/src/components/DebatePost.jsx
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PostComments from './PostComments'
 import PostContent from './PostContent'
 import postData from '../postData'
 
 export default function DebatePost() {
-	const [comments, setComments] = React.useState(postData.comments)
+	const [comments, setComments] = useState(postData.comments)
+	const [username, setUsername] = useState('')
+	const [text, setText] = useState('')
+	const [anonymous, setAnonymous] = useState(false)
+
+	function handleUserName(e) {
+		setUsername(e.target.value)
+	}
+
+	function handleText(e) {
+		setText(e.target.value)
+	}
+	function handleAnonymous(e) {
+		setAnonymous(e.target.checked)
+	}
+
+	function handleComment() {
+		const newComment = {
+			id: comments.length + 1,
+			username: anonymous ? 'Anonymous' : username,
+			text: text,
+		}
+		setComments([...comments, newComment])
+
+		// Clear the form inputs
+		setUsername('')
+		setText('')
+		setAnonymous(false)
+	}
+	// Form submission handler
+	function handleSubmit(e) {
+		e.preventDefault() // Prevent the default form submission behavior
+		handleComment() // Add the new comment
+	}
 
 	return (
 		<div className="post-container">
 			<PostContent data={{ ...postData }} />
 			<PostComments data={comments} />
-			<form>
-				<input className="text-input" type="text" placeholder="Enter username." />
-				<textarea placeholder="What do you think?" />
+			<form onSubmit={handleSubmit}>
+				<input
+					className="text-input"
+					type="text"
+					placeholder="Enter username."
+					value={username}
+					onChange={handleUserName}
+				/>
+				<textarea placeholder="What do you think?" value={text} onChange={handleText} />
 				<label>
-					<input className="checkbox" type="checkbox" />
+					<input className="checkbox" type="checkbox" checked={anonymous} onChange={handleAnonymous} />
 					Post anonymously?
 				</label>
-				<button>Send</button>
+				<button onSubmit={handleComment}>Send</button>
 			</form>
 		</div>
 	)
